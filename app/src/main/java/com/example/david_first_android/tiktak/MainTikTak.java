266793d01710b6f6b82c283c1b890b9784e7e0a6
user <hag.hispin12@gmail.com>
2026-02-18 16:01:02 +0200

    צריך לחבר את האובייקטים לONCLICK

--- app/src/main/java/com/example/david_first_android/tiktak/MainTikTak.java
+++ app/src/main/java/com/example/david_first_android/tiktak/MainTikTak.java
@@ -1,8 +1,10 @@
 package com.example.david_first_android.tiktak;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,26 +19,48 @@ public class MainTikTak extends AppCompatActivity {
      Tile tile1_1, tile1_2, tile1_3;
      Tile tile2_1, tile2_2, tile2_3;
      Tile tile3_1, tile3_2, tile3_3;
+     TextView TV_winner;
+
+    private boolean isWinner(int playerType) {
+        // שורות
+        if (tile1_1.getType() == playerType && tile1_2.getType() == playerType && tile1_3.getType() == playerType)
+            return true;
+        if (tile2_1.getType() == playerType && tile2_2.getType() == playerType && tile2_3.getType() == playerType)
+            return true;
+        if (tile3_1.getType() == playerType && tile3_2.getType() == playerType && tile3_3.getType() == playerType)
+            return true;
+
+        // עמודות
+        if (tile1_1.getType() == playerType && tile2_1.getType() == playerType && tile3_1.getType() == playerType)
+            return true;
+        if (tile1_2.getType() == playerType && tile2_2.getType() == playerType && tile3_2.getType() == playerType)
+            return true;
+        if (tile1_3.getType() == playerType && tile2_3.getType() == playerType && tile3_3.getType() == playerType)
+            return true;
+
+        // אלכסונים
+        if (tile1_1.getType() == playerType && tile2_2.getType() == playerType && tile3_3.getType() == playerType)
+            return true;
+        if (tile1_3.getType() == playerType && tile2_2.getType() == playerType && tile3_1.getType() == playerType)
+            return true;
+
+        return false; // אין מנצח
+    }
 
 
 
 
 
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_tik_tak);
 
-        ImageView IM1 = findViewById(R.id.tile1_1);
-        IM1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-            }
-        });
-
-        // type = 0, res = 0
-        tile1_1 = new Tile(IM1);
+        TV_winner = findViewById(R.id.TV_winner);
+        tile1_1 = new Tile(findViewById(R.id.tile1_1));
         tile1_2 = new Tile(findViewById(R.id.tile1_2));
         tile1_3 = new Tile(findViewById(R.id.tile1_3));
 
@@ -49,12 +73,127 @@ public class MainTikTak extends AppCompatActivity {
         tile3_3 = new Tile(findViewById(R.id.tile3_3));
 
 
+// --- ONCLICK ---
+
+        tile1_1.getIv().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(tile1_1.getType() == 0) {
+                    tile1_1.getIv().setImageResource(R.drawable.goodx);
+                    tile1_1.setType(1);
+                }
+                if(isWinner(tile1_1.getType())){
+                    TV_winner.setText("YOU WIN!!!");
+                }
 
 
+            }
+        });
 
+        tile1_2.getIv().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(tile1_2.getType() == 0) {
+                    tile1_2.getIv().setImageResource(R.drawable.goodx);
+                    tile1_2.setType(1);
+                }
+                if(isWinner(tile1_2.getType())){
+                    TV_winner.setText("YOU WIN!!!");
+                }
+            }
+        });
 
+        tile1_3.getIv().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(tile1_3.getType() == 0) {
+                    tile1_3.getIv().setImageResource(R.drawable.goodx);
+                    tile1_3.setType(1);
+                }
+                if(isWinner(tile1_3.getType())){
+                    TV_winner.setText("YOU WIN!!!");
+                }
+            }
+        });
 
+        tile2_1.getIv().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(tile2_1.getType() == 0) {
+                    tile2_1.getIv().setImageResource(R.drawable.goodx);
+                    tile2_1.setType(1);
+                }
+                if(isWinner(tile2_1.getType())){
+                    TV_winner.setText("YOU WIN!!!");
+                }
+            }
+        });
 
+        tile2_2.getIv().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(tile2_2.getType() == 0) {
+                    tile2_2.getIv().setImageResource(R.drawable.goodx);
+                    tile2_2.setType(1);
+                }
+                if(isWinner(tile2_2.getType())){
+                    TV_winner.setText("YOU WIN!!!");
+                }
+            }
+        });
+
+        tile2_3.getIv().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(tile2_3.getType() == 0) {
+                    tile2_3.getIv().setImageResource(R.drawable.goodx);
+                    tile2_3.setType(1);
+                }
+                if(isWinner(tile2_3.getType())){
+                    TV_winner.setText("YOU WIN!!!");
+                }
+            }
+        });
+
+        tile3_1.getIv().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(tile3_1.getType() == 0) {
+                    tile3_1.getIv().setImageResource(R.drawable.goodx);
+                    tile3_1.setType(1);
+                }
+                if(isWinner(tile3_1.getType())){
+                    TV_winner.setText("YOU WIN!!!");
+                }
+            }
+        });
+
+        tile3_2.getIv().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(tile3_2.getType() == 0) {
+                    tile3_2.getIv().setImageResource(R.drawable.goodx);
+                    tile3_2.setType(1);
+                }
+                if(isWinner(tile3_2.getType())){
+                    TV_winner.setText("YOU WIN!!!");
+                }
+            }
+        });
+
+        tile3_3.getIv().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(tile3_3.getType() == 0) {
+                    tile3_3.getIv().setImageResource(R.drawable.goodx);
+                    tile3_3.setType(1);
+                }
+                if(isWinner(tile3_3.getType())){
+                    TV_winner.setText("YOU WIN!!!");
+                }
+            }
+        });
 
     }
 }
